b255d0b81db534fc65110794424816bdee1de357
Petyo Nachev <pyo@abv.bg>
2021-12-31 12:40:03 +0200

    fix: import

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import "../scss/app.scss";
-const R = require('ramda');
+import * as R from '../../node_modules/ramda/dist/ramda.js';
+/* const R = require('ramda'); */
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
